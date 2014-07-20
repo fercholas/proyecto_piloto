b308ad248d2e8b739b7beb2f311a15eccdc9a23b
Miguel Ramos <miguel9ramos@gmail.com>
2014-07-19 22:00:42 -0300

    Vedades variadas

--- nico_es_gay.js
+++ nico_es_gay.js
@@ -1,6 +1,6 @@
-var nico = 'gay';
-
+var nico = 'gay'
+var secondCharacteristic = 'marica'
 for(var i = 0; i < 15; i++)
 {
-	console.log('Nico es ' + nico);
+	console.log('Nico es muy ' + (i % 2 === 0 ? nico : secondCharacteristic));
 }
